cbe71379d17f2332c90138bd0d05a4a16aa7fe71
jalexandresouza <jalexandresouza@gmail.com>
2025-03-12 23:14:05 -0300

    Commit 2

---
+++ src/test/java/com/alex/cepbackend/CepBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alex.cepbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CepBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
